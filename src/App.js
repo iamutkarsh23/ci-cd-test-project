f697ec59ed1ab6c9ea0291a3502b1e2e12cac272
Utkarsh Patadia <mindutkarsh@gmail.com>
2020-05-17 21:39:47 -0700

    Cleaning up cloudbuild file

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+require('dotenv').config()
+
+class App extends React.Component {
+
+  render() {
+    const dotenv = require('dotenv')
+const buf = Buffer.from('BASIC=basic')
+    const config = dotenv.parse(process.env) // will return an object
+    console.log(typeof config, config) // object { BASIC : 'basic' }
+    console.log(process.env.project_id) 
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
