72307caa7b01a4f1feda6e1c75e75adb61c73795
Utkarsh Patadia <mindutkarsh@gmail.com>
2020-07-23 20:15:23 -0700

    added function and logger

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,34 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const bunyan = require('bunyan');
+
+// Imports the Google Cloud client library for Bunyan
+const {LoggingBunyan} = require('@google-cloud/logging-bunyan');
+
+// Creates a Bunyan Stackdriver Logging client
+const loggingBunyan = new LoggingBunyan();
+
+// Create a Bunyan logger that streams to Stackdriver Logging
+// Logs will be written to: "projects/YOUR_PROJECT_ID/logs/bunyan_log"
+const logger = bunyan.createLogger({
+  // The JSON payload of the log as it appears in Stackdriver Logging
+  // will contain "name": "my-service"
+  name: 'my-service',
+  streams: [
+    // Log to the console at 'info' and above
+    {stream: process.stdout, level: 'info'},
+    // And log to Stackdriver Logging, logging at 'info' and above
+    loggingBunyan.stream('info'),
+  ],
+});
+
+// Writes some log entries
+logger.error('warp nacelles offline');
+logger.info('shields at 99%');
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    logger.info("Hello world called")
+ response.send("Hello from Firebase!");
+});
