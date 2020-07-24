12f255a7e924bd0bf74383c4eafd729a5f73b16b
Utkarsh Patadia <mindutkarsh@gmail.com>
2020-07-23 23:17:39 -0700

    added two loggers

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,9 @@ const bunyan = require('bunyan');
 const {LoggingBunyan} = require('@google-cloud/logging-bunyan');
 
 // Creates a Bunyan Stackdriver Logging client
-const loggingBunyan = new LoggingBunyan();
+const loggingBunyan = new LoggingBunyan({
+    logName: 'info_log'
+});
 
 // Create a Bunyan logger that streams to Stackdriver Logging
 // Logs will be written to: "projects/YOUR_PROJECT_ID/logs/bunyan_log"
@@ -20,17 +22,35 @@ const logger = bunyan.createLogger({
   name: 'my-service',
   streams: [
     // Log to the console at 'info' and above
-    {stream: process.stdout, level: 'info'},
+    // {stream: process.stdout, level: 'info'},
     // And log to Stackdriver Logging, logging at 'info' and above
     loggingBunyan.stream('info'),
   ],
 });
+
+const newLoggingBunyan = new LoggingBunyan({
+    logName: 'warning_log'
+});
+
+const warnlogger = bunyan.createLogger({
+    // The JSON payload of the log as it appears in Stackdriver Logging
+    // will contain "name": "my-service"
+    name: 'warnLogg',
+    streams: [
+      // Log to the console at 'info' and above
+      // {stream: process.stdout, level: 'info'},
+      // And log to Stackdriver Logging, logging at 'info' and above
+      newLoggingBunyan.stream('info'),
+    ],
+  });
 
 // Writes some log entries
 logger.error('warp nacelles offline');
 logger.info('shields at 99%');
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    logger.info("Hello world called")
+
+    warnlogger.warn("first warning")
+    logger.info("Hello world called nowww")
  response.send("Hello from Firebase!");
 });
